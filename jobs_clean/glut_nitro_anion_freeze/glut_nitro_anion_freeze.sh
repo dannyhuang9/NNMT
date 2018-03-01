68faa60bd59e31d4e7c2ef4f45e25f0464cc330b
Danny Huang <dannyhuang818@gmail.com>
2018-02-28 19:41:56 -0500

    stuff

---
+++ jobs_clean/glut_nitro_anion_freeze/glut_nitro_anion_freeze.sh
@@ -0,0 +1,49 @@
+#!/bin/bash 
+
+#SBATCH -N 1        # number of nodes
+
+#SBATCH -n 4   # number of cores 
+
+#SBATCH -p shared   # partition to submit to 
+
+#SBATCH --mem=4096  # memory per node in MB (see also --mem-per-cpu)
+
+#SBATCH -t 300 # expected runtime in minutes
+
+#SBATCH -J g09_glut_nitro_anion_freeze # name of this job
+
+# This is a template script for submitting Gaussian jobs
+# to SLURM.  Tags starting with @ will be replaced with
+# a script.  Eugene Kwan, May 2014
+
+# swap to local node
+mkdir /scratch/dhuang9_glut_nitro_anion_freeze/
+export GAUSS_SCRDIR=/scratch/dhuang9_glut_nitro_anion_freeze
+
+# prevent core dumps on job failure
+ulimit -c 0
+
+# write out when and where the job started
+
+echo "*************************************" > log.txt
+echo "Running on host:"
+hostname >> log.txt
+echo "Job glut_nitro_anion_freeze started at..." >> log.txt
+date >> log.txt
+
+# run job
+g16 glut_nitro_anion_freeze.gjf glut_nitro_anion_freeze.out
+
+# remove temporary directory
+rm -rf /scratch/dhuang9_glut_nitro_anion_freeze
+
+# analyze the result
+./analyze.sh glut_nitro_anion_freeze.out >> log.txt
+
+# add it to the master log
+cat log.txt >> ../output/output.txt
+
+# move completed job to output directory
+mv glut_nitro_anion_freeze.out "../output/"
+
+
